8be2e5db5c5a7174f182728a5430f26244ca0e65
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-03 10:53:27 +0200

    clean up project add Greeting component

---
+++ src/components/Greeting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  data() {
+    return {
+      msg: "hello world!",
+    };
+  },
+};
+</script>
